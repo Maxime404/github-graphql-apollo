d5f9dad24ff88bb532d0d94601ba497aa702080f
Maxime404 <galissairemaxime24@gmail.com>
2020-10-25 01:55:04 +0200

    WIP - Attemp to get some datas

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,120 @@
+import React, { Component } from 'react'
 import './../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Profil from './Profil'
+import Profile from './Profile'
 
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 
-const PROFIL = gql`
-  {
-    user(login: "Maxime404") {
-        repositories {
-          totalCount
-        },
-        avatarUrl,
-        bio,
-        name
-      }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      viewer: {},
+      profileData: ""
+    }
   }
-`;
 
-function App(props) {
+  componentDidMount() {
+    this.getViewer();
+  }
 
-  props.query({
+  getViewer() {
+    this.props.query({
       query: gql`
-      query {
-        viewer {
-          login
+        query {
+          viewer {
+            login
+            id
+          }
+        }
+    `})
+      .then(result => {
+        const viewer = result.data.viewer;
+
+        this.setState({ viewer });
+        this.setAllQueries();
+      });
+  }
+
+  setAllQueries() {
+    this.setProfileQuery();
+  }
+
+  setProfileQuery() {
+    this.props.query({
+      query: gql`
+      {
+        user(login: "${this.state.viewer.login}") {
+            repositories {
+              totalCount
+            },
+            name,
+            bio,
+            avatarUrl
+          }
+      }
+    `}).then(result => {
+        this.setState({ profileData: result.data });
+      });
+  }
+
+  render() {
+
+    const a = gql`
+    query { 
+      user(login: "Maxime404") {
+        repositories(first: 100) {
+          totalCount,
+          nodes {
+            nameWithOwner
+            updatedAt
+            createdAt
+            primaryLanguage {
+              name
+            }
+            languages(first: 100) {
+              nodes {
+                name
+              }
+            }
+          }
         }
       }
-    `
-    })
-    .then(result => console.log(result));
+    }`
 
-  return (
-    <div>
-      <div style={{ position: "absolute", top: 0, left: 0, right: 0, height: "50px", backgroundColor: "#89d4e8" }}></div>
-      <div style={{ position: "absolute", top: 50, left: 0, right: 0, height: "285px", backgroundColor: "#F0F4F1" }}>
-        <div class="container">
-          <div class="mt-5">
-            <Query query={PROFIL} variables={{}}>
-              {({ data, loading }) =>
-                loading ? (
-                  <span>I am loading your data...</span>
-                ) : (
-                    <Profil data={data} />
-                  )
-              }
-            </Query>
+    const b = gql`
+    query { 
+      repository(name: "trocify-front", owner: "Maxime404") {
+        ref(qualifiedName: "master") {
+            target {
+                ... on Commit {
+                    history(author: {id: "MDQ6VXNlcjQ1Nzk0NjM0"}) {
+                        totalCount
+                        nodes {
+                            authoredDate
+                        }
+                    }
+                }
+            } 
+          }
+        }
+      }`
+
+
+    return (
+      <div>
+        <div style={{ position: "absolute", top: 0, left: 0, right: 0, height: "50px", backgroundColor: "#89d4e8" }}></div>
+        <div style={{ position: "absolute", top: 50, left: 0, right: 0, height: "285px", backgroundColor: "#F0F4F1" }}>
+          <div class="container">
+            <div class="mt-5">
+              <Profile data={this.state.profileData} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-  );
+    );
+  }
 }
-
-export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../App.css';
+
+function Profil(props) {
+    
+    return props.data ? (
+        <div>
+            <div class="row">
+                <p class="col-md-6 align-self-end title">{props.data.user.name}</p>
+                <p class="col-md-6 align-self-end">{props.data.user.bio}</p>
+            </div>
+            <div class="row">
+                <img class="col-md-2 align-self-end avatar" src={props.data.user.avatarUrl} alt={props.data.user.name}/>
+            </div>
+        </div>
+    ) : 
+    (
+        <div>Loading data...</div>
+    )
+}
+
+export default Profil;
